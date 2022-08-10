e3c99065fea52f6228e6a81ac84e6fa642864927
Seiji Umatani <umatani@gmail.com>
2022-08-10 10:43:25 +0900

    delete abs/naive.old

--- abs/naive.old/struct-phases.rkt
+++
@@ -1,8 +0,0 @@
-#lang racket
-(require (except-in "struct-core.rkt"
-                    Stxξ Stxξ? Stxξ-stx Stxξ-ξ))
-(provide (all-from-out "struct-core.rkt")
-         (all-defined-out))
-
-;; updated (ph scps)
-(struct Stxξ (ph stx ξ scps) #:transparent)
